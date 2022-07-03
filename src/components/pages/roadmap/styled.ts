445a996285b95e014064650713fb16a26e2c326a
JaimeJesusSerrano <jaimejesusserrano@gmail.com>
2022-07-03 16:47:02 +0200

    [UPDATE] styles to center ReactRoadmapMilestones component

--- src/components/pages/roadmap/styled.ts
+++ src/components/pages/roadmap/styled.ts
@@ -2,14 +2,17 @@ import styled from 'styled-components'
 
 export const BoardWrapper = styled.div`
   display: flex;
+  height: 800px;
+  max-height: 800px;
+  max-width: 1000px;
   overflow-x: auto;
   overflow-y: hidden;
-  width: 100%;
+  width: 1000px;
 `
 
 export const Wrapper = styled.div`
+  align-items: center;
   display: flex;
-  flex: 1;
+  justify-content: center;
   height: calc(100% - 32px);
-  overflow-y: hidden;
 `
