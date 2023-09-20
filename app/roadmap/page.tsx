881f9c8712d4345f54097442665f9c5041ed19f6
JaimeJesusSerrano <jaimejesusserrano@gmail.com>
2023-09-20 15:55:52 +0200

    feat: full screen in mobile

--- app/roadmap/page.tsx
+++ app/roadmap/page.tsx
@@ -9,7 +9,7 @@ import { getById } from '../services/roadmap-milestone'
 const Roadmap = (): JSX.Element => {
   return (
     <div className='h-full w-full flex flex-column content-center justify-center'>
-      <div className='h-5/6 w-5/6 max-w-5/6 overflow-auto mt-8'>
+      <div className='md:h-5/6 md:w-5/6 md:max-w-5/6 overflow-auto md:mt-8'>
         <ReactRoadmapMilestones roadmapMilestonesData={getById(1)} />
       </div>
     </div>
