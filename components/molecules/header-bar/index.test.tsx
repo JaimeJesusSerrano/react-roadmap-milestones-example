eaf0a371efc3fa82b9f919350f07cee0683cc257
JaimeJesusSerrano <jaimejesusserrano@gmail.com>
2023-09-22 15:41:25 +0200

    test: github-action v4

--- components/molecules/header-bar/index.test.tsx
+++ components/molecules/header-bar/index.test.tsx
@@ -7,6 +7,6 @@ import ClientComponent from './index'
 
 it('Component: HeaderBar', () => {
   render(<ClientComponent />)
-  const headerBarTitle = screen.queryByText('React Roadmap Milestones Example test')
+  const headerBarTitle = screen.queryByText('React Roadmap Milestones Example')
   expect(headerBarTitle).toBeTruthy()
 })
