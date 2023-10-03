f852f2146a00fe7fac568318c5d937035c6d73d0
JaimeJesusSerrano <jaimejesusserrano@gmail.com>
2023-10-03 10:46:58 +0200

    fix: mobile breakpoint

--- app/[lang]/roadmap/page.tsx
+++ app/[lang]/roadmap/page.tsx
@@ -9,7 +9,7 @@ import { getExample } from '@/app/services/roadmap-milestone'
 const Roadmap = (): JSX.Element => {
   return (
     <div className="flex h-full w-full content-center justify-center">
-      <div className="overflow-auto md:mt-8 md:h-5/6 md:w-5/6">
+      <div className="h-full w-full overflow-auto md:mt-8 md:h-5/6 md:w-5/6">
         <ReactRoadmapMilestones roadmapMilestonesData={getExample()} />
       </div>
     </div>
